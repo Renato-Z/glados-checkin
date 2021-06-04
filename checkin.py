5791796795fc1107461a54ee14da277138781d88
zren <50518861+Renato-Z@users.noreply.github.com>
2021-06-04 15:55:56 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -44,9 +44,9 @@ def start():
         time = time.split('.')[0]
         #print(time)
         if sever == 'on':
-            requests.post('http://pushplus.hxtrip.com/send',{token:sckey,title:'GLaDOS 签到提醒',content: mess +'，会员剩余' + time + '天'})
+            requests.post('http://pushplus.hxtrip.com/send',{'token':sckey,'title':'GLaDOS 签到提醒','content': mess +'，会员剩余' + time + '天'})
     else:
-        requests.post('http://pushplus.hxtrip.com/send',{token:sckey,title:'GLaDOS Cookie过期'})
+        requests.post('http://pushplus.hxtrip.com/send',{'token':sckey,'title':'GLaDOS Cookie过期'})
 
 def main_handler(event, context):
   return start()
